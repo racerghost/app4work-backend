1637ef1dd350cd27e098b5a7bab9e8fb1ce562d3
Carlos Bausá Carmona <litos@MacBook-Pro-de-Carlos.local>
2022-03-11 21:10:52 +0100

    CRUD User

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require("dotenv/config"); //para que coja las variables del fichero de .env
+require("dotenv/config"); //para que coja las constiables del fichero de .env
 require("./db");            //para que inicialize el fichero de la base de datos
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.model')
 /* GET users listing. */
-router.get('/pp', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async function(req, res, next) {
+  try {
+    const getusers=await User.find({})
+    res.json(getusers);
+  }catch(error){
+    console.log(error);
+  }
 });
 
+/* GET single user listing. */
+router.get('/:id', async function(req, res, next) {
+  const {id}= req.params;
+  try {
+    const user = await User.findById(id);
+    res.json(user);
+  }catch(error){
+    console.log(error);
+  }
+});
+
+
+/* create users listing. */
+router.post('/', async function(req, res, next) {
+  const {email, username,password}=req.body;
+  try {
+    const createdUser=await User.create({email,username,password})
+    res.json(createdUser);
+  }catch(error){
+    console.log(error);
+  }
+});
+
+
+
+/* DELETE users listing. */
+router.delete('/delete/:id', async function(req, res, next) {
+  const {id} = req.params;
+  try {
+    const deleteuser=await User.findByIdAndDelete(id);
+    res.json(deleteuser);
+  }catch(error){
+    console.log(error);
+  }
+
+});
+
+
+/* EDIT user. */
+router.put('/edit/:id', async function(req, res, next) {
+  const { id } = req.params;
+  const { email, username, password } = req.body;
+  try {
+    const editedUser = await User.findByIdAndUpdate(id, { email, username, password },{new:true});
+    res.json(editedUser);
+  }catch(error){
+    console.log(error);
+  }
+});
 module.exports = router;
