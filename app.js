a66e97f9bfef26febbbf77009d8aab5da3a38aa9
Carlos Bausá Carmona <litos@MacBook-Pro-de-Carlos.local>
2022-05-04 18:51:26 +0200

    add cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
- const cors = require('cors');
+const cors = require('cors');
 
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
